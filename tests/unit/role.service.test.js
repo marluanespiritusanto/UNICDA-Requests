4e7876df0ead32ffb55903ea2a4de526de012d09
Marluan <marluanespiritusantoguerrero@gmail.com>
2019-07-26 00:24:01 -0400

    imp: preparing the first test

--- tests/unit/role.service.test.js
+++ tests/unit/role.service.test.js
@@ -1,9 +1,24 @@
-const { RoleServiceMock } = require("../mocks");
+const { RoleRepository } = require("../../src/repositories");
+const mockingoose = require("mockingoose").default;
+const { Role } = require("../../src/models");
 
 describe("Role Service", () => {
-  beforeEach(async () => {});
+  beforeEach(() => {
+    mockingoose.resetAll();
+    jest.clearAllMocks();
+  });
 
-  it("should find a role", () => {
-    const role = { id: 1, name: "GENERIC", status: "ACTIVE" };
+  it("should find a role", async () => {
+    const role = {
+      _id: 1,
+      name: "GENERIC",
+      status: "ACTIVE"
+    };
+
+    mockingoose(Role).toReturn(role, "findOne");
+
+    const _roleRepository = new RoleRepository({ Role });
+
+    expect((await _roleRepository.get(1)).name).toBe(role.name);
   });
 });
