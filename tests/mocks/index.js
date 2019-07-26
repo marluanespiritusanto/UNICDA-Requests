61749892ad5bd32951a767f383cc340826645c88
Marluan <marluanespiritusantoguerrero@gmail.com>
2019-07-25 23:52:39 -0400

    add:  installing jest and configuring tests

---
+++ tests/mocks/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  RoleServiceMock: require("./role.service.mock")
+};
---
+++ tests/unit/role.service.test.js
@@ -0,0 +1,9 @@
+const { RoleServiceMock } = require("../mocks");
+
+describe("Role Service", () => {
+  beforeEach(async () => {});
+
+  it("should find a role", () => {
+    const role = { id: 1, name: "GENERIC", status: "ACTIVE" };
+  });
+});
