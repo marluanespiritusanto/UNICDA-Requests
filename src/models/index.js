0cb81a912a3b8ff1e5b6507de5454dd74df3fc23
Marluan <marluanespiritusantoguerrero@gmail.com>
2019-07-29 11:41:36 -0400

    del: Request Type model deleted

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,6 @@ module.exports = {
   User: require("./user.model"),
   Role: require("./role.model"),
   Request: require("./request.model"),
-  RequestType: require("./request-type.model"),
   RequestForm: require("./request-form.model"),
   FormType: require("./form-types.model")
 };
