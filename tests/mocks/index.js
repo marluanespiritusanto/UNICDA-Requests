f6bccf90119b0a97149d607b947bdc07ce41acf9
Marluan <marluanespiritusantoguerrero@gmail.com>
2019-07-29 19:16:56 -0400

    del: request tests

--- tests/mocks/index.js
+++ tests/mocks/index.js
@@ -3,7 +3,5 @@ module.exports = {
   RoleRepositoryMock: require("./role/role.repository.mock"),
   UserModelMock: require("./user/user.model.mock"),
   UserRepositoryMock: require("./user/user.repository.mock"),
-  RequestModelMock: require("./request/request.model.mock"),
-  RequestRepositoryMock: require("./request/request.repository.mock"),
   AuthRepositoryMock: require("./auth/auth.repository.mock")
 };
--- tests/mocks/request/request.repository.mock.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  get: jest.fn(),
-  getAll: jest.fn(),
-  create: jest.fn(),
-  update: jest.fn(),
-  delete: jest.fn(),
-  getRequestByName: jest.fn()
-};
