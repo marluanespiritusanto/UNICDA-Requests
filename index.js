7eb23d22542a30144e694017261f6ef283f6078b
Marluan <marluanespiritusantoguerrero@gmail.com>
2019-07-30 21:42:24 -0400

    fix: useFindAndModify

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const mongoose = require("mongoose");
 mongoose.set("useCreateIndex", true);
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: true })
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => server.start())
   .catch(console.log);
